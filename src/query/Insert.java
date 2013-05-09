1e3d732ef8e25cbbfef932fc10f5fa3a58fc4ae5
raedselim <raedaselim@gmail.com>
2013-05-09 21:04:24 +0200

    insert initial code

--- src/query/Insert.java
+++ src/query/Insert.java
@@ -1,29 +1,58 @@
 package query;
 
+import global.Minibase;
+import global.RID;
+import global.SearchKey;
+import heap.HeapFile;
+import index.HashIndex;
 import parser.AST_Insert;
+import relop.Schema;
+import relop.Tuple;
 
 /**
  * Execution plan for inserting tuples.
  */
 class Insert implements Plan {
 
-  /**
-   * Optimizes the plan, given the parsed query.
-   * 
-   * @throws QueryException if table doesn't exists or values are invalid
-   */
-  public Insert(AST_Insert tree) throws QueryException {
+	String fileName;
+	Object[] values;
 
-  } // public Insert(AST_Insert tree) throws QueryException
+	/**
+	 * Optimizes the plan, given the parsed query.
+	 * 
+	 * @throws QueryException
+	 *             if table doesn't exists or values are invalid
+	 */
+	public Insert(AST_Insert tree) throws QueryException
+	{
+		fileName = tree.getFileName();
+		values = tree.getValues();
+	} // public Insert(AST_Insert tree) throws QueryException
 
-  /**
-   * Executes the plan and prints applicable output.
-   */
-  public void execute() {
+	/**
+	 * Executes the plan and prints applicable output.
+	 */
+	public void execute()
+	{
+		// insert values into table
+		HeapFile f = new HeapFile(fileName);
+		Schema schema = Minibase.SystemCatalog.getSchema(fileName);
+		Tuple tuple = new Tuple(schema, values);
+		RID rid = f.insertRecord(tuple.getData());
 
-    // print the output message
-    System.out.println("0 rows affected. (Not implemented)");
+		// update indexes of this table
+		IndexDesc[] indexes = Minibase.SystemCatalog.getIndexes(fileName);
+		for (IndexDesc ind : indexes)
+		{
+			String indexName = ind.indexName;
+			String colName = ind.columnName;
+			HashIndex index = new HashIndex(indexName);
+			SearchKey key = new SearchKey(tuple.getField(colName));
+			index.insertEntry(key, rid);
+		}
 
-  } // public void execute()
+		// print the output message
+		System.out.println("1 row affected.");
 
+	} // public void execute()
 } // class Insert implements Plan
