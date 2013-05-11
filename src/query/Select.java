6a8c0a0979f7e6ba37cf159cf9e3fcb91e2fda17
Mahmoud Radwan <m.radwan92@hotmail.com>
2013-05-12 00:16:13 +0200

    push down selection starting v0.0000000

--- src/query/Select.java
+++ src/query/Select.java
@@ -25,6 +25,7 @@ class Select implements Plan {
 	String[] cols, tables;
 	SortKey[] orders;
 	Predicate[][] preds;
+	Iterator[] tablesIters;
 
 	/**
 	 * Optimizes the plan, given the parsed query.
@@ -32,32 +33,44 @@ class Select implements Plan {
 	 * @throws QueryException
 	 *             if validation fails
 	 */
-	public Select(AST_Select tree) throws QueryException
-	{
+	public Select(AST_Select tree) throws QueryException {
 		cols = tree.getColumns();
 		orders = tree.getOrders();
 		preds = tree.getPredicates();
 		tables = tree.getTables();
 	} // public Select(AST_Select tree) throws QueryException
 
+	public void push() {
+
+		for (int i = 0; i < preds.length; i++) {
+			for (int j = 0; j < tables.length; j++) {
+				Schema s = Minibase.SystemCatalog.getSchema(tables[i]);
+				boolean match = true;
+				for (int k = 0; k < preds[i].length && match; k++)
+					match &= preds[i][k].validate(s);
+				if (match) {
+					// put selection p(table[i]) in the iterators array instead
+					// of table[i]
+				}
+			}
+		}
+	}
+
 	/**
 	 * Executes the plan and prints applicable output.
 	 */
-	public void execute()
-	{
+	public void execute() {
 		// FileScan[] scans = new FileScan[tables.length];
 
 		// opening tables scans
 		Queue<Iterator> queue = new LinkedList<Iterator>();
-		for (int i = 0; i < tables.length; i++)
-		{
+		for (int i = 0; i < tables.length; i++) {
 			Schema schema = Minibase.SystemCatalog.getSchema(tables[i]);
 			HeapFile file = new HeapFile(tables[i]);
 			queue.add(new FileScan(schema, file));
 		}
 		// join all tables (super join)
-		while (queue.size() > 1)
-		{
+		while (queue.size() > 1) {
 			Iterator left = queue.poll();
 			Iterator right = queue.poll();
 			queue.add(new SimpleJoin(left, right, new Predicate[0]));
@@ -65,16 +78,14 @@ class Select implements Plan {
 		Iterator selected = queue.poll();
 
 		// select from tables
-		for (int i = 0; i < preds.length; i++)
-		{
+		for (int i = 0; i < preds.length; i++) {
 			Iterator[] ors = new Iterator[preds[i].length];
-			for (int j = 0; j < preds[i].length; j++)
-			{
-				 ors[j] = new Selection(selected, preds[i][j]);
-//				 System.out.println(preds[i][j].toString());
+			for (int j = 0; j < preds[i].length; j++) {
+				ors[j] = new Selection(selected, preds[i][j]);
+				// System.out.println(preds[i][j].toString());
 			}
-//			System.out.println("----------------");
-			selected = new Union(ors , i);
+			// System.out.println("----------------");
+			selected = new Union(ors, i);
 		}
 		// projection
 
@@ -86,14 +97,13 @@ class Select implements Plan {
 
 		Projection output = new Projection(selected, fields);
 		int count = 0;
-		while (output.hasNext())
-		{
+		while (output.hasNext()) {
 			output.getNext().print();
 			count++;
 		}
-//		output.explain(0);
+		// output.explain(0);
 		output.close();
-		
+
 		// print the output message
 		System.out.println(count + " rows affected.");
 
@@ -102,56 +112,49 @@ class Select implements Plan {
 	class Union extends Iterator {
 		Iterator[] itrs;
 		int next;
-		public Union(Iterator[] itrs, int t)
-		{
+
+		public Union(Iterator[] itrs, int t) {
 			setSchema(itrs[0].getSchema());
 			this.itrs = itrs;
 			next = 0;
 		}
 
 		@Override
-		public void explain(int depth)
-		{
+		public void explain(int depth) {
 			System.out.println("Union : " + depth);
-			for(int i = 0 ; i < itrs.length;i++)
-				itrs[i].explain(depth+1);
+			for (int i = 0; i < itrs.length; i++)
+				itrs[i].explain(depth + 1);
 		}
 
 		@Override
-		public void restart()
-		{
-			if(next == itrs.length)
+		public void restart() {
+			if (next == itrs.length)
 				next--;
-			while(next >= 0)
+			while (next >= 0)
 				itrs[next--].restart();
 			next = 0;
 		}
 
 		@Override
-		public boolean isOpen()
-		{
+		public boolean isOpen() {
 			return itrs[0].isOpen();
 		}
 
 		@Override
-		public void close()
-		{
+		public void close() {
 			for (int i = 0; i < itrs.length; i++)
 				itrs[i].close();
 		}
 
 		@Override
-		public boolean hasNext()
-		{
+		public boolean hasNext() {
 			return next < itrs.length && itrs[next].hasNext();
 		}
 
 		@Override
-		public Tuple getNext()
-		{
+		public Tuple getNext() {
 			Tuple ret = itrs[next].getNext();
-			if(!itrs[next].hasNext())
-			{
+			if (!itrs[next].hasNext()) {
 				itrs[next].restart();
 				next++;
 			}
